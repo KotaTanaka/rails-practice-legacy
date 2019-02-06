f691d63c522356aeb2f705227ccc5f5474f38670
KotaTanaka <te11k.tanaka@icloud.com>
2019-02-07 01:14:10 +0900

    learning: Progate Rails5 03-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  get "/" => "home#top"
-  get "about" => "home#about"
+  # 店舗一覧取得
+  get "shops" => "shops#index"
+  # 店舗詳細取得
+  get "shops/:id" => "shops#show"
+  # レビュー一覧取得
   get "reviews" => "reviews#index"
+  # ホーム画面
+  get "/" => "home#top"
+  # 紹介ページ
+  get "about" => "home#about"
   resources :spots
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
