16d148335e68509f6fecb061806ebc52f339a673
Kota Tanaka <te11k.tanaka@gmail.com>
2019-03-10 21:45:07 +0900

    refactor: 例外ハンドリング・レスポンス生成モジュールの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@
 # 基底コントローラー
 ####################
 class ApplicationController < ActionController::Base
+  include RenderJsonResponse
+
+  # その他の例外
+  rescue_from Exception, :with => :internal_server_error
+
+  def not_found_error
+    render_not_found
+  end
+  
+  def internal_server_error e=nil
+    logger.error e.message
+    e.backtrace.each { |line| logger.error line }
+    render_internal_server_error(e.message)
+  end
 end
