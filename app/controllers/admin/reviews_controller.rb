3c092c9c2b9ad90b82f664de8b7b3c5558cef18c
Kota Tanaka <te11k.tanaka@gmail.com>
2019-03-14 02:03:38 +0900

    feat: 管理レビュー一覧取得API正常系

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -2,19 +2,27 @@
 # 管理API レビューコントローラー
 ####################
 class Admin::ReviewsController < Admin::ApplicationController
+  include ValidateCondition
+  include RenderReviewsResponse
+
   # レビュー一覧取得
   # GET /admin/reviews
+  # GET /admin/reviews.html
   def index
     @reviews = Review.all.order(created_at: :desc)
+      .limit(validate_limit(params[:limit]))
+      .offset(validate_offset(params[:offset]))
+      .order(created_at: validate_sort(params[:sort]))
 
     respond_to do |format|
-      format.json { render json: @reviews }
+      format.json { render_review_list(Review.count, @reviews) }
       format.html { render("reviews/index") }
     end
   end
 
   # レビューステータス変更
   # PUT /admin/reviews/:id
+  # PUT /admin/reviews/:id.html
   def update
     @review = Review.find_by(id: params[:id])
     # TODO ステータス変更処理
@@ -31,6 +39,7 @@ class Admin::ReviewsController < Admin::ApplicationController
 
   # レビュー削除
   # DELETE /admin/reviews/:id
+  # DELETE /admin/reviews/:id.html
   def destroy
     @review = Review.find_by(id: params[:id])
     @review.destroy
